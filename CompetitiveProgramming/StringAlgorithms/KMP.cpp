71d4c7495545344491ab156f5f9845b0f06b32bc
singhsanket143 <singhsanket143@gmail.com>
2019-05-05 03:39:27 +0530

    Added codes

--- CompetitiveProgramming/StringAlgorithms/KMP.cpp
+++ CompetitiveProgramming/StringAlgorithms/KMP.cpp
@@ -1,45 +1,48 @@
 #include <iostream>
-#define MAX_N 100010
-int preprocess[MAX_N];
+#define MAX_N 100005
+int reset[MAX_N];
 using namespace std;
 
 void KMPpreprocess(string pat) {
-	int i = 0, j = -1; 
-	preprocess[0] = -1;
-	while(i < pat.length()) {
-		while(j >= 0 and pat[i] != pat[j]) j = preprocess[j];
-		i++;j++;
-		preprocess[i] = j;
+	int i = 0, j = 1;
+	reset[0] = -1;
+	while(i < pat.size()) {
+		// Check for resetting
+		while(j >= 0 and pat[i]!=pat[j]) {
+			j = reset[j];
+		}
+		i++;
+		j++;
+		reset[i] = j;
 	}
-	for(int i=0;i<pat.length()+1;i++) {
-		cout<<preprocess[i]<<" ";
-	}
-	cout<<endl;
 }
 
 void KMPsearch(string str, string pat) {
 	KMPpreprocess(pat);
 	int i = 0, j = 0;
-	while(i < str.length()) {
-		while(j>=0 and str[i] != pat[j]) {
-		    j = preprocess[j];
+	while(i < str.size()) {
+		while(j >= 0 and str[i] != pat[j]) {
+			j = reset[j];
 		}
-		i++; j++;
-		if (j == pat.length())
-		{
-			cout<<"Pattern is found at index "<<i-j<<endl;
-			j = preprocess[j];
+		i++;
+		j++;
+		if(j == pat.size()) {
+			cout<<"Pattern is found at"<<i-j<<endl;
+			j = reset[j];
 		}
 	}
 }
 
+
 int main(int argc, char const *argv[])
 {
-	for(int i=0;i<MAX_N;i++) {
-		preprocess[i] = -1;
+	/* code */
+	for(int i = 0; i < MAX_N; i++) {
+		reset[i] = -1;
 	}
 	string str, pat;
 	cin>>str>>pat;
 	KMPsearch(str, pat);
 	return 0;
 }
+
