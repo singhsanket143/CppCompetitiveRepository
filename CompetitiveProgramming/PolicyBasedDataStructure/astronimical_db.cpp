1c9cc21c37273a84703d32ccc68386141ec8afbc
Sanket Singh <singhsanket143@gmail.com>
2020-11-02 10:57:50 +0530

    Add files via upload

---
+++ CompetitiveProgramming/PolicyBasedDataStructure/astronimical_db.cpp
@@ -0,0 +1,71 @@
+// Problem Link - https://acm.timus.ru/problem.aspx?space=1&num=1414
+/* By Sanket Singh */
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/trie_policy.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+#define ll 				long long int
+#define ld				long double
+#define mod             1000000007
+#define inf             1e18
+#define endl			"\n"
+#define pb 				emplace_back
+#define vi              vector<ll>
+#define vs				vector<string>
+#define pii             pair<ll,ll>
+#define ump				unordered_map
+#define mp 				map
+#define pq_max          priority_queue<ll>
+#define pq_min          priority_queue<ll,vi,greater<ll> >
+#define ff 				first
+#define ss 				second
+#define mid(l,r)        (l+(r-l)/2)
+#define loop(i,a,b) 	for(int i=(a);i<=(b);i++)
+#define looprev(i,a,b) 	for(int i=(a);i>=(b);i--)
+#define log(args...) 	{ string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;	
+#define token(str,ch)	(std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;)
+vs tokenizer(string str,char ch) {std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;}
+
+
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cout << *it << " = " << a << endl;
+	err(++it, args...);
+}
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main(int argc, char const *argv[]) {
+	file_i_o();
+	pbtrie tr;
+	tr.insert("sun");
+	string str;
+	while(cin>>str) {
+		if(str[0] == '?') {
+			cout<<str.substr(1)<<endl;
+			auto prefix_strings = tr.prefix_range(str.substr(1));
+			int count = 0;
+			for(auto it = prefix_strings.first;count<20&& it!=prefix_strings.second;it++, count++){
+				cout<<"  "<<*it<<endl;
+			}
+		} else {
+			tr.insert(str.substr(1));
+		}
+	}
+	return 0;
+}
