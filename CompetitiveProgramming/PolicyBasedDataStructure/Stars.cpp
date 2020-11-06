7dbaabe46645bf4a6220db14d615671b3334ef3f
singhsanket143 <singhsanket143@gmail.com>
2020-11-06 19:33:21 +0530

    Added 2 ptr code

--- CompetitiveProgramming/PolicyBasedDataStructure/Stars.cpp
+++ CompetitiveProgramming/PolicyBasedDataStructure/Stars.cpp
@@ -1,38 +1,32 @@
-<snippet>
-	<content><![CDATA[
-// Problem Link - 
+// Problem Link - https://acm.timus.ru/problem.aspx?space=1&num=1028
 /* By Sanket Singh */
 #include<bits/stdc++.h>
-//#include<ext/pb_ds/assoc_container.hpp>
-//#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
 //#include <ext/pb_ds/trie_policy.hpp>
-//using namespace __gnu_pbds;
+using namespace __gnu_pbds;
 using namespace std;
 #define ll 				long long int
 #define ld				long double
 #define mod             1000000007
 #define inf             1e18
 #define endl			"\n"
-#define pb 				push_back
+#define pb 				emplace_back
 #define vi              vector<ll>
 #define vs				vector<string>
 #define pii             pair<ll,ll>
 #define ump				unordered_map
-#define mp 				make_pair
+#define mp 				map
 #define pq_max          priority_queue<ll>
 #define pq_min          priority_queue<ll,vi,greater<ll> >
-#define all(n) 			n.begin(),n.end()
 #define ff 				first
 #define ss 				second
 #define mid(l,r)        (l+(r-l)/2)
-#define bitc(n) 		__builtin_popcount(n)
 #define loop(i,a,b) 	for(int i=(a);i<=(b);i++)
 #define looprev(i,a,b) 	for(int i=(a);i>=(b);i--)
-#define iter(container, it) for(__typeof(container.begin()) it = container.begin(); it != container.end(); it++)
 #define log(args...) 	{ string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
 #define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;	
-template <typename T> T gcd(T a, T b){if(a%b) return gcd(b,a%b);return b;}
-template <typename T> T lcm(T a, T b){return (a*(b/gcd(a,b)));}
+#define token(str,ch)	(std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;)
 vs tokenizer(string str,char ch) {std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;}
 
 
@@ -42,7 +36,7 @@ void err(istream_iterator<string> it, T a, Args... args) {
 	cout << *it << " = " << a << endl;
 	err(++it, args...);
 }
-//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 //typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
 
 void file_i_o()
@@ -60,17 +54,25 @@ int main(int argc, char const *argv[]) {
 	clock_t begin = clock();
 	file_i_o();
 	// Write your code here....
-
+	int n;
+    int sz=0;
+    cin>>n;
+    vector<int> ans(n,0);
+ 
+    pbds t;
+    int x,y;
+    for(int i=0;i<n;i++)
+    {
+        cin>>x>>y;
+        ans[t.order_of_key({x,++sz})]++;
+        t.insert({x,sz});
+    }
+ 
+    for(int i=0;i<n;i++)
+        cout<<ans[i]<<'\n';
 	#ifndef ONLINE_JUDGE 
 	  clock_t end = clock();
 	  cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
 	#endif 
 	return 0;
 }
-
-]]></content>
-	<!-- Optional: Set a tabTrigger to define how to trigger the snippet -->
-<tabTrigger>cpp</tabTrigger>
-	<!-- Optional: Set a scope to limit where the snippet will trigger -->
-<!-- <scope>source.cpp</scope> -->
-</snippet>
