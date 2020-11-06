29cc722d5a59a8e224f6635827c147f9d0f468d5
singhsanket143 <singhsanket143@gmail.com>
2020-11-06 19:08:20 +0530

    Added 2 ptr code

---
+++ CompetitiveProgramming/Two_Pointer/Vasya_And_String.cpp
@@ -0,0 +1,95 @@
+// Problem Link - https://codeforces.com/problemset/problem/676/C
+/* By Sanket Singh */
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/trie_policy.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+#define ll 				long long int
+#define ld				long double
+#define mod             1000000007
+#define inf             1e18
+#define endl			"\n"
+#define pb 				emplace_back
+#define vi              vector<ll>
+#define vs				vector<string>
+#define pii             pair<ll,ll>
+#define ump				unordered_map
+#define mp 				map
+#define pq_max          priority_queue<ll>
+#define pq_min          priority_queue<ll,vi,greater<ll> >
+#define ff 				first
+#define ss 				second
+#define mid(l,r)        (l+(r-l)/2)
+#define loop(i,a,b) 	for(int i=(a);i<=(b);i++)
+#define looprev(i,a,b) 	for(int i=(a);i>=(b);i--)
+#define log(args...) 	{ string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;	
+#define token(str,ch)	(std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;)
+vs tokenizer(string str,char ch) {std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;}
+
+
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cout << *it << " = " << a << endl;
+	err(++it, args...);
+}
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+//typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+}
+
+ll maxBeauty(string str, int k, char ch) {
+	ll count = 0;
+	ll ans = INT_MIN;
+	ll l = 0, r = 0;
+	while(r < str.size()) {
+		while(r < str.size() and str[r] == ch) {
+			ans = max(ans, r-l+1);
+			r++;
+		}
+		while(r < str.size() and str[r] != ch and count < k) {
+			count++;
+			ans = max(ans, r-l+1);
+			r++;
+		}
+		while(l <= r and str[r] != ch and count >= k) {
+			if(k == 0) {
+				l = ++r;
+				break;
+			}
+			count -= (str[l] != ch);
+			l++;
+		}
+	}
+	return ans;
+}
+
+int main(int argc, char const *argv[]) {
+	clock_t begin = clock();
+	file_i_o();
+	// Write your code here....
+	int n, k;
+	cin>>n>>k;
+	string str;
+	cin>>str;
+	ll x = maxBeauty(str, k, 'a');
+	ll y = maxBeauty(str, k, 'b');
+	cout<<max(x, y);
+	#ifndef ONLINE_JUDGE 
+	  clock_t end = clock();
+	  cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+	#endif 
+	return 0;
+}
