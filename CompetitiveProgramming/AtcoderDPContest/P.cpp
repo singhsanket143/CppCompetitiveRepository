c58deda8f790096308a0600b2f52fd088c6c5ac5
singhsanket143 <singhsanket143@gmail.com>
2021-04-09 18:08:45 +0530

    added codes

---
+++ CompetitiveProgramming/AtcoderDPContest/P.cpp
@@ -0,0 +1,102 @@
+// Problem Link - https://atcoder.jp/contests/dp/tasks/dp_p
+/* By Sanket Singh */
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/trie_policy.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+#define ll 				long long int
+#define ld				long double
+#define mod             1000000007
+#define inf             1e18
+#define endl			"\n"
+#define pb 				push_back
+#define vi              vector<ll>
+#define vs				vector<string>
+#define pii             pair<ll,ll>
+#define ump				unordered_map
+#define mp 				make_pair
+#define pq_max          priority_queue<ll>
+#define pq_min          priority_queue<ll,vi,greater<ll> >
+#define all(n) 			n.begin(),n.end()
+#define ff 				first
+#define ss 				second
+#define mid(l,r)        (l+(r-l)/2)
+#define bitc(n) 		__builtin_popcount(n)
+#define loop(i,a,b) 	for(int i=(a);i<=(b);i++)
+#define looprev(i,a,b) 	for(int i=(a);i>=(b);i--)
+#define iter(container, it) for(__typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define log(args...) 	{ string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;	
+template <typename T> T gcd(T a, T b){if(a%b) return gcd(b,a%b);return b;}
+template <typename T> T lcm(T a, T b){return (a*(b/gcd(a,b)));}
+vs tokenizer(string str,char ch) {std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;}
+
+
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cout << *it << " = " << a << endl;
+	err(++it, args...);
+}
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+//typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+}
+
+vector<int> tree[100005]; // array of vector
+ll dp[100005][2];
+
+ll independentSet(int node, int parent, int c) { // c->constraint/color
+	if(dp[node][c] != -1) return dp[node][c];
+	ll ans = 0;
+	ll w0 = 1;
+	for(auto child : tree[node]) {
+		if(child != parent) {
+			w0 = (w0 * independentSet(child, node, 0)) % mod;
+		}
+	}
+	ans += w0;
+	if(not c) {
+		ll w1 = 1;
+		for(auto child : tree[node]) {
+			if(child != parent) {
+				w1 = (w1 * independentSet(child, node, 1)) % mod;
+			}
+		}
+		ans = (ans%mod + w1%mod)%mod;
+	}
+
+	return dp[node][c] = ans;
+}
+
+int main(int argc, char const *argv[]) {
+	clock_t begin = clock();
+	file_i_o();
+	// Write your code here....
+	int n;
+	cin>>n;
+	memset(dp, -1, sizeof(dp));
+	loop(i, 0, n-2) {
+		int x, y;
+		cin>>x>>y;
+		tree[x].pb(y);
+		tree[y].pb(x);
+	}
+	cout<<independentSet(1, -1, 0);
+	#ifndef ONLINE_JUDGE 
+	  clock_t end = clock();
+	  cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+	#endif 
+	return 0;
+}
--- UnacademyFreeLiveClass/frog.cpp
+++ UnacademyFreeLiveClass/frog.cpp
@@ -1,46 +1,7 @@
-// Problem Link - 
-/* By Sanket Singh */
 #include<bits/stdc++.h>
-//#include<ext/pb_ds/assoc_container.hpp>
-//using namespace __gnu_pbds;
 using namespace std;
-#define ll 				long long int
-#define ld				long double
-#define mod             1000000007
-#define inf             1e18
-#define endl			"\n"
-#define pb 				emplace_back
-#define vi              vector<ll>
-#define vs				vector<string>
-#define pii             pair<ll,ll>
-#define ump				unordered_map
-#define mp 				map
-#define pq_max          priority_queue<ll>
-#define pq_min          priority_queue<ll,vi,greater<ll> >
-#define ff 				first
-#define ss 				second
-#define mid(l,r)        (l+(r-l)/2)
-#define loop(i,a,b) 	for(int i=(a);i<=(b);i++)
-#define looprev(i,a,b) 	for(int i=(a);i>=(b);i--)
-#define log(args...) 	{ string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
-#define logarr(arr,a,b)	for(int i=(a);i<=(b);i++) cout<<(arr[i])<<" ";cout<<endl;	
-#define token(str,ch)	(std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;)
-vs tokenizer(string str,char ch) {std::istringstream var((str)); vs v; string t; while(getline((var), t, (ch))) {v.pb(t);} return v;}
-
-
-void err(istream_iterator<string> it) {}
-template<typename T, typename... Args>
-void err(istream_iterator<string> it, T a, Args... args) {
-	cout << *it << " = " << a << endl;
-	err(++it, args...);
-}
-//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
-
 void file_i_o()
 {
-    ios_base::sync_with_stdio(0); 
-    cin.tie(0); 
-    cout.tie(0);
 	#ifndef ONLINE_JUDGE
 	    freopen("input.txt", "r", stdin);
 	    freopen("output.txt", "w", stdout);
@@ -49,18 +10,28 @@ void file_i_o()
 
 int main(int argc, char const *argv[]) {
 	file_i_o();
+	// Write your code here....
 	int n;
-	cin>>n;
-	vector<int> arr(n+1);
-	loop(i, 1, n) {
-		cin>>arr[i];
+	cin>>n; // takes input of an integer from user
+	int arr[n+1];
+	for(int i = 1; i <= n; i++) {
+		cin>>arr[i]; // takes input in the array
 	}
-	vector<int> dp(n+1, 0);
-	dp[1] = 0;
-	dp[2] = abs(arr[2] - arr[1]);
-	loop(i, 3, n) {
-		dp[i] = min(dp[i-1] + abs(arr[i] - arr[i-1]), dp[i-2] + abs(arr[i] - arr[i-2]));
+	if(n == 1) {
+		cout<<0;
+		return 0;
 	}
-	cout<<dp[n];
+	if(n == 2) {
+		cout<<abs(arr[1] - arr[2]);
+		return 0;
+	}
+	int a = 0, b = abs(arr[1] - arr[2]);
+	int c = -1;
+	for(int i = 3; i <= n; i++) {
+		c = min(a + abs(arr[i] - arr[i-2]), b + abs(arr[i] - arr[i-1]));
+		a = b;
+		b = c;
+	}
+	cout<<c;
 	return 0;
 }
