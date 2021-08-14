6d5ced2d3e7074661492aa0ca7d8ba7d5dd786f4
Sanket Singh <singhsanket143@gmail.com>
2021-08-14 16:57:30 +0530

    Added code tribe codes

---
+++ CodeTribe/DynamicProgrammingCodingRounds/mcoins.cpp
@@ -0,0 +1,90 @@
+// Problem Link - 
+/* By Sanket Singh */
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/trie_policy.hpp>
+//using namespace __gnu_pbds;
+#define ll 				long long int
+#define ld				long double
+#define mod             1000000007
+#define inf             1e18
+#define nl				"\n"
+#define pb 				push_back
+#define eb				emplace_back
+#define vi              std::vector<ll>
+#define vs				std::vector<std::string>
+#define pii             std::pair<ll,ll>
+#define ump				std::unordered_map
+#define mp 				std::make_pair
+#define pq_max          std::priority_queue<ll>
+#define pq_min          std::priority_queue<ll,vi,std::greater<ll> >
+#define all(n) 			n.begin(),n.end()
+#define ff 				first
+#define ss 				second
+#define mid(l,r)        (l+(r-l)/2)
+#define bitc(n) 		__builtin_popcount(n)
+#define loop(i,a,b) 	for(int i=(a);i<=(b);i++)
+#define looprev(i,a,b) 	for(int i=(a);i>=(b);i--)
+#define iter(container, it) for(__typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define log(args...) 	{ std::string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); std::stringstream _ss(_s); std::istream_iterator<std::string> _it(_ss); err(_it, args); }
+#define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) std::cout<<(arr[z])<<" ";std::cout<<std::endl;	
+template <typename T> T gcd(T a, T b){if(a%b) return gcd(b,a%b);return b;}
+template <typename T> T lcm(T a, T b){return (a*(b/gcd(a,b)));}
+vs tokenizer(std::string str,char ch) {std::istringstream var((str)); vs v; std::string t; while(std::getline((var), t, (ch))) {v.pb(t);} return v;}
+
+
+void err(std::istream_iterator<std::string> it) {}
+template<typename T, typename... Args>
+void err(std::istream_iterator<std::string> it, T a, Args... args) {
+    std::cout << *it << " = " << a << std::endl;
+    err(++it, args...);
+}
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+//typedef trie<std::string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+
+void file_i_o()
+{
+    std::ios_base::sync_with_stdio(0); 
+    std::cin.tie(0); 
+    std::cout.tie(0);
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+}
+
+int main(int argc, char const *argv[]) {
+    clock_t begin = clock();
+    file_i_o();
+    // Write your code here....
+    int k, l, m;
+    std::cin>>k>>l>>m;
+
+    std::vector<ll> dp(1000005, 0);
+
+    // base cases i = 1, k, l are winning states for sure
+    dp[1] = 1; 
+    dp[k] = 1;
+    dp[l] = 1;
+
+    for(int i = 2; i < 1000005; i++) {
+        if(i == l or i == k) continue;
+        if(i-1 >= 0 and dp[i-1] == 0) dp[i] = 1;
+        if(i - k >= 0 and dp[i-k] == 0) dp[i] = 1;
+        if(i - l >= 0 and dp[i-l] == 0) dp[i] = 1;
+    }
+
+    while(m--) {
+        int n;
+        std::cin>>n;
+        if(dp[n] == 1) std::cout<<"A";
+        else std::cout<<"B";
+    }
+
+    #ifndef ONLINE_JUDGE 
+      clock_t end = clock();
+      std::cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+    #endif 
+    return 0;
+}
---
+++ UnacademyCourse/NumberTheoryJuly2021/Lecture8/canuhandleweights.cpp
@@ -0,0 +1,123 @@
+// Problem Link - 
+/* By Sanket Singh */
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/trie_policy.hpp>
+//using namespace __gnu_pbds;
+#define ll 				long long int
+#define ld				long double
+#define mod             1000000007
+#define inf             1e18
+#define nl				"\n"
+#define pb 				push_back
+#define eb				emplace_back
+#define vi              std::vector<ll>
+#define vs				std::vector<std::string>
+#define pii             std::pair<ll,ll>
+#define ump				std::unordered_map
+#define mp 				std::make_pair
+#define pq_max          std::priority_queue<ll>
+#define pq_min          std::priority_queue<ll,vi,std::greater<ll> >
+#define all(n) 			n.begin(),n.end()
+#define ff 				first
+#define ss 				second
+#define mid(l,r)        (l+(r-l)/2)
+#define bitc(n) 		__builtin_popcount(n)
+#define loop(i,a,b) 	for(int i=(a);i<=(b);i++)
+#define looprev(i,a,b) 	for(int i=(a);i>=(b);i--)
+#define iter(container, it) for(__typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define log(args...) 	{ std::string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); std::stringstream _ss(_s); std::istream_iterator<std::string> _it(_ss); err(_it, args); }
+#define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) std::cout<<(arr[z])<<" ";std::cout<<std::endl;	
+template <typename T> T gcd(T a, T b){if(a%b) return gcd(b,a%b);return b;}
+template <typename T> T lcm(T a, T b){return (a*(b/gcd(a,b)));}
+vs tokenizer(std::string str,char ch) {std::istringstream var((str)); vs v; std::string t; while(std::getline((var), t, (ch))) {v.pb(t);} return v;}
+
+
+void err(std::istream_iterator<std::string> it) {}
+template<typename T, typename... Args>
+void err(std::istream_iterator<std::string> it, T a, Args... args) {
+    std::cout << *it << " = " << a << std::endl;
+    err(++it, args...);
+}
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+//typedef trie<std::string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+
+void file_i_o()
+{
+    std::ios_base::sync_with_stdio(0); 
+    std::cin.tie(0); 
+    std::cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+class Triplet {
+public:
+    ll x, y, gcd;
+};
+
+Triplet gcdExtended(ll a, ll b) {
+    if(b == 0) {
+        Triplet ans;
+        ans.x = 1;
+        ans.y = 0;
+        ans.gcd = a;
+        return ans;
+    }
+    Triplet ans = gcdExtended(b, a%b);
+    Triplet result;
+    result.gcd = ans.gcd;
+    result.x = ans.y;
+    result.y = ans.x - ((a/b)*(ans.y));
+    return result;
+}
+
+ll modInv(ll a, ll m) {
+    ll x = gcdExtended(a, m).x;
+    return (x%m + m)%m;
+}
+
+int main(int argc, char const *argv[]) {
+    clock_t begin = clock();
+    file_i_o();
+    // Write your code here....
+
+    ll t;
+    std::cin>>t;
+    while(t--) {
+        int a, b, d;
+        std::cin>>a>>b>>d;
+        ll g = gcdExtended(a, b).gcd;
+        if(d%g != 0) {
+            std::cout<<0<<"\n";
+            continue;
+        }
+        if(d == 0) {
+            std::cout<<1<<"\n";
+            continue;
+        }
+        a /= g;
+        b /= g;
+        d /= g;
+        ll y1 = ((d%a)*modInv(b, a))%a;
+        ll firstValue = d/b;
+        if(d < y1*b) {
+            std::cout<<0<<"\n";
+            continue;
+        } else {
+            ll n = (firstValue - y1)/a;
+            std::cout<<(n+1)<<"\n";
+        }
+    }
+
+    #ifndef ONLINE_JUDGE 
+      clock_t end = clock();
+      std::cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+    #endif 
+    return 0;
+}
+
+// ax+by = c;
