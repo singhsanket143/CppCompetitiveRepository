fb6fb4681afd407dcddca4fb63db7ad002bd50a6
singhsanket143 <singhsanket143@gmail.com>
2021-09-13 16:01:25 +0530

    Added codes

---
+++ UnacademyFreeLiveClass/SortingSearchingMicroCourseSep/selection_sort.cpp
@@ -0,0 +1,93 @@
+// Problem Link - 
+/* By Sanket Singh */
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/trie_policy.hpp>
+//using namespace __gnu_pbds;
+#define ll 				long long int
+#define ld				long double
+#define mod             1000000007
+#define inf             1e18
+#define nl				"\n"
+#define pb 				push_back
+#define eb				emplace_back
+#define vi              std::vector<ll>
+#define vs				std::vector<std::string>
+#define pii             std::pair<ll,ll>
+#define ump				std::unordered_map
+#define mp 				std::make_pair
+#define pq_max          std::priority_queue<ll>
+#define pq_min          std::priority_queue<ll,vi,std::greater<ll> >
+#define all(n) 			n.begin(),n.end()
+#define ff 				first
+#define ss 				second
+#define mid(l,r)        (l+(r-l)/2)
+#define bitc(n) 		__builtin_popcount(n)
+#define loop(i,a,b) 	for(int i=(a);i<=(b);i++)
+#define looprev(i,a,b) 	for(int i=(a);i>=(b);i--)
+#define iter(container, it) for(__typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define log(args...) 	{ std::string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); std::stringstream _ss(_s); std::istream_iterator<std::string> _it(_ss); err(_it, args); }
+#define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) std::cout<<(arr[z])<<" ";std::cout<<std::endl;	
+template <typename T> T gcd(T a, T b){if(a%b) return gcd(b,a%b);return b;}
+template <typename T> T lcm(T a, T b){return (a*(b/gcd(a,b)));}
+vs tokenizer(std::string str,char ch) {std::istringstream var((str)); vs v; std::string t; while(std::getline((var), t, (ch))) {v.pb(t);} return v;}
+
+
+void err(std::istream_iterator<std::string> it) {}
+template<typename T, typename... Args>
+void err(std::istream_iterator<std::string> it, T a, Args... args) {
+    std::cout << *it << " = " << a << std::endl;
+    err(++it, args...);
+}
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+//typedef trie<std::string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+
+void file_i_o()
+{
+    std::ios_base::sync_with_stdio(0); 
+    std::cin.tie(0); 
+    std::cout.tie(0);
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+}
+
+// start is the starting point of the unsorted region
+int find_min(std::vector<int> &arr, int start) {
+    int min_index = start;
+    int i = start + 1;
+
+    while(i < arr.size()) {
+        if(arr[i] < arr[min_index]) {
+            min_index = i;
+        }
+        i++;
+    }
+    return min_index;
+}
+
+void selection_sort(std::vector<int> &arr) {
+    for(int i = 0; i < arr.size(); i++) {
+        int min_idx = find_min(arr, i);
+        if(i !=  min_idx) {
+            std::swap(arr[i], arr[min_idx]);
+        }
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    clock_t begin = clock();
+    file_i_o();
+    // Write your code here....
+    std::vector<int> arr {5,4,3,2,1,6, 5, 1};
+    selection_sort(arr);
+    logarr(arr, 0, arr.size() - 1);
+
+    #ifndef ONLINE_JUDGE 
+      clock_t end = clock();
+      std::cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+    #endif 
+    return 0;
+}
