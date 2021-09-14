9c4dc969e278833766689aaa372fc17092ac8f0f
singhsanket143 <singhsanket143@gmail.com>
2021-09-14 15:58:02 +0530

    Added codes

---
+++ UnacademyCourse/RecursionBacktrackingINtroDPCPAug21/Lecture11/knapsack1.cpp
@@ -0,0 +1,128 @@
+// Problem Link - 
+/* By Sanket Singh */
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/trie_policy.hpp>
+//using namespace __gnu_pbds;
+#define ll 				long long int
+#define ld				long double
+#define mod             1000000007
+#define inf             1e18
+#define nl				"\n"
+#define pb 				push_back
+#define eb				emplace_back
+#define vi              std::vector<ll>
+#define vs				std::vector<std::string>
+#define pii             std::pair<ll,ll>
+#define ump				std::unordered_map
+#define mp 				std::make_pair
+#define pq_max          std::priority_queue<ll>
+#define pq_min          std::priority_queue<ll,vi,std::greater<ll> >
+#define all(n) 			n.begin(),n.end()
+#define ff 				first
+#define ss 				second
+#define mid(l,r)        (l+(r-l)/2)
+#define bitc(n) 		__builtin_popcount(n)
+#define loop(i,a,b) 	for(int i=(a);i<=(b);i++)
+#define looprev(i,a,b) 	for(int i=(a);i>=(b);i--)
+#define iter(container, it) for(__typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define log(args...) 	{ std::string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); std::stringstream _ss(_s); std::istream_iterator<std::string> _it(_ss); err(_it, args); }
+#define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) std::cout<<(arr[z])<<" ";std::cout<<std::endl;	
+template <typename T> T gcd(T a, T b){if(a%b) return gcd(b,a%b);return b;}
+template <typename T> T lcm(T a, T b){return (a*(b/gcd(a,b)));}
+vs tokenizer(std::string str,char ch) {std::istringstream var((str)); vs v; std::string t; while(std::getline((var), t, (ch))) {v.pb(t);} return v;}
+
+
+void err(std::istream_iterator<std::string> it) {}
+template<typename T, typename... Args>
+void err(std::istream_iterator<std::string> it, T a, Args... args) {
+    std::cout << *it << " = " << a << std::endl;
+    err(++it, args...);
+}
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+//typedef trie<std::string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+
+void file_i_o()
+{
+    std::ios_base::sync_with_stdio(0); 
+    std::cin.tie(0); 
+    std::cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+std::vector<ll> wt;
+std::vector<ll> c;
+ll dp[105][100005];
+
+ll knapsack(int i, int W, int n) {
+    if(i == n) return 0;
+    if(W <= 0) return 0;
+    if(dp[i][W] != -1) return dp[i][W];
+    // pick
+    ll result = 0;
+    ll f1 = 0;
+    if(wt[i] <= W) {
+        f1 = c[i] + knapsack(i+1, W - wt[i], n);
+    }
+    ll f2 = knapsack(i+1, W, n);
+    result = std::max(f1, f2);
+
+    return dp[i][W] = result;
+}
+
+ll knapsackBU(int W, int n) {
+    std::vector<std::vector<ll> > dp(n+1, std::vector<ll> (W+1, 0));
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= W; j++) {
+            dp[i][j] = dp[i-1][j]; // not pick
+            if(wt[i] <= j) {
+                dp[i][j] = std::max(dp[i][j], c[i] + dp[i-1][j-wt[i]]);
+            }
+        }
+    }
+    return dp[n][W];
+}
+
+ll knapsackBUbtr(int W, int n) {
+    vi dp1(W+1, 0);
+    vi dp2(W+1, 0);
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= W; j++) {
+            dp2[j] = dp1[j];
+            if(wt[i] <= j) {
+                dp2[j] = std::max(dp1[j], c[i] + dp1[j-wt[i]]);
+            }
+        }
+        dp2.swap(dp1);
+        dp2.clear();
+    }
+    return dp1[W];
+}
+
+int main(int argc, char const *argv[]) {
+    clock_t begin = clock();
+    file_i_o();
+    // Write your code here....
+    std::memset(dp, -1, sizeof dp);
+    int n, W;
+    std::cin>>n>>W;
+    wt.pb(0);
+    c.pb(0);
+    loop(i, 0, n-1) {
+        ll w, cost;
+        std::cin>>w>>cost;
+        wt.pb(w);
+        c.pb(cost);
+    }
+    // std::cout<<knapsack(0, W, n);
+    std::cout<<knapsackBUbtr(W, n);
+    #ifndef ONLINE_JUDGE 
+      clock_t end = clock();
+      std::cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+    #endif 
+    return 0;
+}
