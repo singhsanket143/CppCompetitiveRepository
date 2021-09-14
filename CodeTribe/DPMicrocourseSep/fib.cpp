9e4f169d4a5982e0143e512cde106b982416fe9c
singhsanket143 <singhsanket143@gmail.com>
2021-09-14 19:03:27 +0530

    Added codes

---
+++ CodeTribe/DPMicrocourseSep/fib.cpp
@@ -0,0 +1,91 @@
+// Problem Link - 
+/* By Sanket Singh */
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//#include <ext/pb_ds/trie_policy.hpp>
+//using namespace __gnu_pbds;
+#define ll 				long long int
+#define ld				long double
+#define mod             1000000007
+#define inf             1e18
+#define nl				"\n"
+#define pb 				push_back
+#define eb				emplace_back
+#define vi              std::vector<ll>
+#define vs				std::vector<std::string>
+#define pii             std::pair<ll,ll>
+#define ump				std::unordered_map
+#define mp 				std::make_pair
+#define pq_max          std::priority_queue<ll>
+#define pq_min          std::priority_queue<ll,vi,std::greater<ll> >
+#define all(n) 			n.begin(),n.end()
+#define ff 				first
+#define ss 				second
+#define mid(l,r)        (l+(r-l)/2)
+#define bitc(n) 		__builtin_popcount(n)
+#define loop(i,a,b) 	for(int i=(a);i<=(b);i++)
+#define looprev(i,a,b) 	for(int i=(a);i>=(b);i--)
+#define iter(container, it) for(__typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define log(args...) 	{ std::string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); std::stringstream _ss(_s); std::istream_iterator<std::string> _it(_ss); err(_it, args); }
+#define logarr(arr,a,b)	for(int z=(a);z<=(b);z++) std::cout<<(arr[z])<<" ";std::cout<<std::endl;	
+template <typename T> T gcd(T a, T b){if(a%b) return gcd(b,a%b);return b;}
+template <typename T> T lcm(T a, T b){return (a*(b/gcd(a,b)));}
+vs tokenizer(std::string str,char ch) {std::istringstream var((str)); vs v; std::string t; while(std::getline((var), t, (ch))) {v.pb(t);} return v;}
+
+
+void err(std::istream_iterator<std::string> it) {}
+template<typename T, typename... Args>
+void err(std::istream_iterator<std::string> it, T a, Args... args) {
+    std::cout << *it << " = " << a << std::endl;
+    err(++it, args...);
+}
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+//typedef trie<std::string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+
+void file_i_o()
+{
+    std::ios_base::sync_with_stdio(0); 
+    std::cin.tie(0); 
+    std::cout.tie(0);
+}
+
+// recursive solution
+int fib(int n) {
+    if(n == 0 or n == 1) return n;
+    return fib(n-1) + fib(n-2);
+}
+
+// TD DP
+int dp[100];
+int fibTD(int n) {
+    if(n == 0 or n == 1) return n;
+    if(dp[n] != -1) return dp[n]; // if the value is not -1 then it is precomputed don't go on it again
+    return dp[n] = fibTD(n-1) + fibTD(n-2);
+}
+
+int fibBU(int n) {
+    std::vector<int> dp1(n+1, 0);
+    dp1[0] = 0; // base case
+    dp1[1] = 1; // base case
+    for(int i = 2; i <= n; i++) {
+        dp1[i] = dp1[i-1] + dp1[i-2];
+    }
+    return dp1[n];
+}
+
+int main(int argc, char const *argv[]) {
+    clock_t begin = clock();
+    file_i_o();
+    // Write your code here....
+
+    std::memset(dp, -1, sizeof dp);
+    log(fib(5), fibTD(5), fibBU(5));
+
+
+    #ifndef ONLINE_JUDGE 
+      clock_t end = clock();
+      std::cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
+    #endif 
+    return 0;
+}
